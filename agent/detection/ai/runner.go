4e429d33a68caf07f178a05e9ac596df00c17d51
Luca Coratu <lucacoratu@gmail.com>
2025-04-06 20:41:46 +0300

    Added rules for SSTI detection in python apps which use jinja2

---
+++ agent/detection/ai/runner.go
@@ -0,0 +1,21 @@
+package detection
+
+import (
+	"net/http"
+
+	"github.com/lucacoratu/disertatie/agent/logging"
+)
+
+type AIClassifierRunner struct {
+	logger logging.ILogger
+}
+
+func NewAIClassifierRunner(logger logging.ILogger) *AIClassifierRunner {
+	return &AIClassifierRunner{logger: logger}
+}
+
+func (vr *AIClassifierRunner) RunAIClassifierOnRequest(r *http.Request) {
+}
+
+func (vr *AIClassifierRunner) RunAIClassifierOnResponse(r *http.Response) {
+}
